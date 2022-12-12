d5662b27226877d78ecc305105c4129896f2e23a
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-12 23:36:01 +0200

    production endpoint fix

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,3 @@
 export const API_ENDPOINT_PATH = process.env.REACT_APP_HOST_TYPE === 'production' ?
-    "https://dev-ezulu.herokuapp.com" :
+    "https://master.d1vvvi88g40rh8.amplifyapp.com" :
     "http://localhost:4000/api/editor"
