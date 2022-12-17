42192919f8cab1eb7e8323962b1d4f2a0495918f
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-17 20:30:11 +0200

    REACT_APP_TYPE

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
-export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production' ?
+export const API_ENDPOINT_PATH = process.env.REACT_APP_HOST_TYPE === 'production' ?
     "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor" :
     "http://localhost:4000/api/editor"
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.REACT_APP_TYPE)
