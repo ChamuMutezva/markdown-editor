345bed23912b7de9665342e9948c292d13f0e1ee
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-17 17:25:01 +0200

    using process.env.NODE_ENV

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,5 @@
-export const API_ENDPOINT_PATH = process.env.REACT_APP_HOST_TYPE === 'production' ?
+export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production' ?
     "https://dev.d3f1qjpgfbf286.amplifyapp.com" :
     "http://localhost:4000/api/editor"
+
+console.log(process.env.NODE_ENV)
