03904d73d0c8889f593771a07a08849c88112998
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-19 00:37:04 +0200

    using React-app_type

--- src/config.tsx
+++ src/config.tsx
@@ -2,7 +2,6 @@ export const API_ENDPOINT_PATH = process.env.REACT_APP_TYPE !== 'production'
   ? "http://localhost:4000/api/editor"
   : "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
 
-console.log(API_ENDPOINT_PATH)
-// console.log(process.env.REACT_APP_TYPE)
-// console.log(process.env.NODE_ENV)
-// console.log(process.env.REACT_APP_API_URL)
+console.log(process.env.REACT_APP_TYPE)
+console.log(process.env.NODE_ENV)
+console.log(process.env.REACT_APP_API_URL)
