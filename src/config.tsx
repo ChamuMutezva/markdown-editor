60f48067b078d78692c4479b859c47a5df20745d
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-18 16:07:13 +0200

    fix: errors caused by using NODE_ENV

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,7 @@
-export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production' ?
-    "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor" :
-    "http://localhost:4000/api/editor"
+export const API_ENDPOINT_PATH = process.env.NODE_ENV !== 'production'
+  ? process.env.REACT_APP_API_URL || "http://localhost:4000/api/editor"
+  : "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
 
 console.log(process.env.REACT_APP_TYPE)
 console.log(process.env.NODE_ENV)
+console.log(process.env.REACT_APP_API_URL)
