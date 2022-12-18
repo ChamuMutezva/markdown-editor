d7a0ed204363cf02cecda35055c1e7312cf4b409
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-18 20:10:47 +0200

    Data not showing on production - trial fix

--- src/config.tsx
+++ src/config.tsx
@@ -1,7 +1,8 @@
 export const API_ENDPOINT_PATH = process.env.REACT_APP_TYPE !== 'production'
-  ? process.env.REACT_APP_API_URL || "http://localhost:4000/api/editor"
+  ? "http://localhost:4000/api/editor"
   : "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
 
-console.log(process.env.REACT_APP_TYPE)
-console.log(process.env.NODE_ENV)
-console.log(process.env.REACT_APP_API_URL)
+console.log(API_ENDPOINT_PATH)
+// console.log(process.env.REACT_APP_TYPE)
+// console.log(process.env.NODE_ENV)
+// console.log(process.env.REACT_APP_API_URL)
