29a4c945468b4fab6ff936b5e065c2f31ebc1878
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-24 14:50:31 +0200

    return to using aws amplify url

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,5 @@
 export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
-    ? "https://markdown-editor-ckm.netlify.app/api/editor"
+    ? "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
     : "http://localhost:4000/api/editor"
 
 console.log(process.env.NODE_ENV)
-//"https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
