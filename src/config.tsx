17820fa042a72c3e37a0b4106be4492dd33bc299
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-24 14:44:59 +0200

    netlify url trial

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,6 @@
 export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
-    ? "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
+    ? "https://markdown-editor-ckm.netlify.app/api/editor"
     : "http://localhost:4000/api/editor"
 
 console.log(process.env.NODE_ENV)
+//"https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
