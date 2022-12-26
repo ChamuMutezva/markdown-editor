366226cdbd0f78e70871be8bfb401f4abe59da46
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-26 21:14:30 +0200

    append 'api/editor' on url

--- src/config.tsx
+++ src/config.tsx
@@ -7,6 +7,6 @@ export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
 // eslint-disable-next-line import/prefer-default-export
 export const API_ENDPOINT_PATH =
   process.env.NODE_ENV === "production"
-    ? "https://markdown-editor-ckm.netlify.app"
+    ? "https://markdown-editor-ckm.netlify.app/api/editor"
     : "http://localhost:4000/api/editor";
 console.log(process.env.NODE_ENV);
