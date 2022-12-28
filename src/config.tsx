2511f410e877482d42e6ccb01038de5952adfb50
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-28 12:06:05 +0200

    aws link url application

--- src/config.tsx
+++ src/config.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line import/prefer-default-export
-/*
 export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
     ? "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
     : "http://localhost:4000/api/editor"
-*/
+/*
 // eslint-disable-next-line import/prefer-default-export
 export const API_ENDPOINT_PATH =
   process.env.NODE_ENV === "production"
     ? "https://markdown-editor-ckm.netlify.app/api/editor"
     : "http://localhost:4000/api/editor";
+    */
 console.log(process.env.NODE_ENV);
