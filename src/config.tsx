b54ace254f32e07dccc8bdcbad202e1a8dd244ff
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-29 17:25:04 +0200

    remove current amplify configuration

--- src/config.tsx
+++ src/config.tsx
@@ -1,16 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
-/*
-export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
-    ? "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
-    : "http://localhost:4000/api/editor"
-/*
-// eslint-disable-next-line import/prefer-default-export
-export const API_ENDPOINT_PATH =
-  process.env.NODE_ENV === "production"
-    ? "https://markdown-editor-ckm.netlify.app/api/editor"
-    : "http://localhost:4000/api/editor";
-    */
-
+//  "https://markdown-editor-ckm.netlify.app/api/editor" 
 // eslint-disable-next-line import/prefer-default-export
 export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
     ? "https://lime-smoggy-camel.cyclic.app/"
