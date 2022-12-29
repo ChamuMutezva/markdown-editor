58bdb705dacd1f9a471fe0be6bfa65690434e80d
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-29 15:57:32 +0200

    deployment on cyclic

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
+/*
 export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
     ? "https://dev.d3f1qjpgfbf286.amplifyapp.com/api/editor"
     : "http://localhost:4000/api/editor"
@@ -9,4 +10,9 @@ export const API_ENDPOINT_PATH =
     ? "https://markdown-editor-ckm.netlify.app/api/editor"
     : "http://localhost:4000/api/editor";
     */
-console.log(process.env.NODE_ENV);
+
+// eslint-disable-next-line import/prefer-default-export
+export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
+    ? "https://lime-smoggy-camel.cyclic.app/"
+    : "http://localhost:4000/api/editor"
+    console.log(process.env.NODE_ENV);
