b040d3e6d7bf9267a12eccf022a9b45780e7acdf
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-31 19:53:06 +0200

    pwa dev-dist folder auto generated

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser',
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser",
       },
-    ]
-  }
-})
+    ],
+  },
+});
