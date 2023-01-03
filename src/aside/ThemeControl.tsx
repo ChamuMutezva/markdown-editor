1d6f5909aef985415d28bc9c1fb83d9b512be360
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-01-03 21:56:22 +0200

    theme switch adjusted to match the design

--- src/aside/ThemeControl.tsx
+++ src/aside/ThemeControl.tsx
@@ -1,25 +1,44 @@
-import React, { useContext } from 'react'
-import LightMode from "../assets/icon-light-mode.svg"
-import DarkMode from "../assets/icon-dark-mode.svg"
-import { ThemeContext } from '../context/ThemeContext'
+import React, { useContext } from "react";
+import LightMode from "../assets/icon-light-mode.svg";
+import DarkMode from "../assets/icon-dark-mode.svg";
+import { ThemeContext } from "../context/ThemeContext";
 
 function ThemeControl(props: { setExpand: () => void }) {
+  const { theme, onChangeTheme } = useContext(ThemeContext);
 
-    const { theme, onChangeTheme } = useContext(ThemeContext)
+  const onChange = () => {
+    onChangeTheme?.(theme);
+    // eslint-disable-next-line react/destructuring-assignment
+     props.setExpand();
+  };
 
-    const onChange = () => {
-        onChangeTheme?.(theme)
-        // eslint-disable-next-line react/destructuring-assignment
-        props.setExpand()
-    }
-
-    return (
-        <button type='button' className='btn btn-theme-control' aria-pressed={theme} onClick={onChange}>
-            <span className='sr-only'>light theme</span>
-            <img className={`light-theme-img ${theme ? "hide-theme-img" : ""}`} src={DarkMode} alt="" />
-            <img className={`dark-theme-img ${theme ? "" : "hide-theme-img"}`} src={LightMode} alt="" />
-        </button>
-    )
+  return (
+    <button
+      type="button"
+      role="switch"
+      className="btn btn-theme-control"
+      aria-checked={theme}
+      aria-label="toggle dark mode"
+      onClick={onChange}
+    >
+      <span className="check">
+        <span className="theme-icons">
+          <img
+            className={`light-theme-img ${theme ? "hide-theme-img" : ""}`}
+            src={DarkMode}
+            aria-hidden
+            alt=""
+          />
+          <img
+            className={`dark-theme-img ${theme ? "" : "hide-theme-img"}`}
+            src={LightMode}
+            aria-hidden
+            alt=""
+          />
+        </span>
+      </span>
+    </button>
+  );
 }
 
-export default ThemeControl
+export default ThemeControl;
