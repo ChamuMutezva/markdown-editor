bea5b5bb582c3580e99a9c848121dd93f983922b
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-01-17 03:16:07 +0200

    update: pwa - installable improvements

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,31 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Markdown editor preview app",
+        short_name: "Markdown",
+        description: "Markdown editor and preview application - write in markdown mode and preview the results",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+      },
     }),
   ],
   resolve: {
