8a5c92c34e68b52ca86c203cb68f3c3759fca404
Chamu Mutezva <ckmutezva@gmail.com>
2024-06-29 07:51:28 +0200

    link for render - backend deployed on render

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,8 @@
-//  "https://markdown-editor-ckm.netlify.app/api/editor" 
+//  "https://markdown-editor-ckm.netlify.app/api/editor"
 // eslint-disable-next-line import/prefer-default-export
-export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
-    ? "https://lime-smoggy-camel.cyclic.app/"
-    : "http://localhost:4000/api/editor"
-    console.log(process.env.NODE_ENV);
+export const API_ENDPOINT_PATH =
+    process.env.NODE_ENV === "production"
+        ? "https://markdown-editor-back.onrender.com/"
+        : "http://localhost:4000/api/editor";
+
+// "https://lime-smoggy-camel.cyclic.app/"
