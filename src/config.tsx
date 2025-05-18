ed34ebcf2358ce952dc2b144c7eff21558ae795d
Chamu Mutezva <ckmutezva@gmail.com>
2025-05-18 09:11:48 +0200

    fix: remove the / on address api endpoint

--- src/config.tsx
+++ src/config.tsx
@@ -2,7 +2,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const API_ENDPOINT_PATH =
     process.env.NODE_ENV === "production"
-        ? "https://markdown-editor-back.onrender.com/"
+        ? "https://markdown-editor-back.onrender.com"
         : "http://localhost:4000/api/editor";
 
 // "https://lime-smoggy-camel.cyclic.app/"
